501d674163ae91d86ac2a7d601d920f6b6653c0a
Knight-Of-Rueful-Countenance <irag500@york.ac.uk>
2021-06-08 02:42:41 +0100

    Added code to load a gif, and replace the background with a slightly broken rainbow

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # gif_rainbowify
+
 Imagemagick based command line and dynamic linked library solution to add animated rainbow effects to gifs
+
+## Building
+
+- You can get the Magick++ libraries from here: https://download.imagemagick.org/ImageMagick/download/binaries/
+  I got this one ImageMagick-6.9.12-14-Q16-HDRI-x64-dll.exe because the demos won't working cause they were looking for CORE*RL_magick*.lib
+  rather than seems CORE*RL_MagickCore*.lib as is present in 7.0, so for simplicity I used the older version.
+- Next, open up the visual studio project, and point it towards the directories where the lib and header files for imagemagick are included
--- gif_rainbowify/gif_rainbowify.cpp
+++ gif_rainbowify/gif_rainbowify.cpp
@@ -1,20 +1,103 @@
 // gif_rainbowify.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
+#define IMAGE "B:\\Users\\Idris\\Pictures\\1489175712017a.gif"
+#define IMAGE_OUT "B:\\Users\\Idris\\Pictures\\1489175712017a_cropped.gif"
+#define RAINBOW_IMAGE_PATH "C:\\Users\\Idris\\source\\repos\\gif_rainbowify\\rainbow.png"
 
-int main()
+
+
+#include <Magick++.h>
+#include <iostream> 
+
+using namespace std;
+using namespace Magick;
+
+int main(int argc, char** argv)
 {
-    std::cout << "Hello World!\n";
+    InitializeMagick(*argv);
+
+    // Construct the image object. Seperating image construction from the 
+    // the read operation ensures that a failure to read the image file 
+    // doesn't render the image object useless. 
+    list<Image> imageList;
+
+    // TODO define rainbow image based on page size here, and crop it where it is currently defined.
+    
+    try {
+        // Read each frame of the GIF 
+        readImages(&imageList, IMAGE);
+
+        // build a new image but with the different height
+        list<Image> newGif;
+
+        // Load a rainbow image from a file.
+        // Resize it to match the image dimensions of the gif image we are loading.
+        Image base_rainbow;
+        base_rainbow.read(RAINBOW_IMAGE_PATH);
+
+        // Scale to fit the existing image.
+        Geometry resizeParams = imageList.front().page();
+        resizeParams.aspect(true);
+        base_rainbow.resize(resizeParams);
+
+        // Define chosen colour range for transparency (e.g. which pixels will be rainbowified:
+        // NOTE: transparencyChroma is only really suitable for colours that are similar (e.g. are monotonic across channels).
+        Color startColour = Color("dim gray"), endColour = Color("white");
+        double hue = 100;
+        
+        for (Image image : imageList) {
+            // We want the sections that were originally transparent to remain transparent.
+            // We can do this by CopyOpacityCompositeOp to copy the opacity from the original image 
+            Image originalImage = image;
+
+            // Make the sections of image we want transparent
+            image.transparentChroma(startColour, endColour);
+
+            // Define the rainbow image. Note that the page size and the number of columns and rows are different. 
+            Image rainbow_image = base_rainbow;
+            // Chagne the hue by 20%
+            rainbow_image.modulate(100, 100, hue);
+            hue+=20;
+
+            //rainbow_image.crop(image.page());
+
+            // Copy the animation delay from the previous image
+            rainbow_image.animationDelay(originalImage.animationDelay());
+
+            // Crop/Scale the rainbow image to the size of the image.
+            rainbow_image.composite(image, SouthEastGravity, OverCompositeOp);
+
+            // Copy the original image opacity back so those sections aren't blue.
+            rainbow_image.composite(originalImage, SouthEastGravity, CopyOpacityCompositeOp);
+
+            // Copy the original image page information so the image is drawn in the right place.
+            rainbow_image.page(originalImage.page());
+            rainbow_image.repage();
+
+            newGif.push_back(rainbow_image);
+        }
+
+        // Write the image to a file 
+        writeImages(newGif.begin(), newGif.end(), IMAGE_OUT);
+    }
+    catch (Exception& error_)
+    {
+        cout << "Caught exception: " << error_.what() << endl;
+        return 1;
+    }
+
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+// TODO: 
+// animate the rainbow effect.
+// this can be done by adjusting the hue of the rainbow image.
+// Also give an option for repeating textures  (e.g. like the gold moving left to right) by moving left to right with them.
+// then add Command line interface for easy access.
+// figure out what to if we want the rainbow animation to have a different number of frames than the initial gif (e.g. for 1 frame gifs, etc.).
+//  maybe repeat the gif if we keep the framerate the same, or add new frames.
+
+// looks like you need to put more thought into this:
+   // you need to keep trackof the rainbow positions in previous images that are being carried over to the current frame as we need to animate them there as well.

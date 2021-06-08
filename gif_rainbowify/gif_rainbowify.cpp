511cf4b9fbf945008fd8be6c1e9bd22e6e714180
Knight-Of-Rueful-Countenance <irag500@york.ac.uk>
2021-06-08 15:47:30 +0100

    Got basic rainbowification of gifs working

--- gif_rainbowify/gif_rainbowify.cpp
+++ gif_rainbowify/gif_rainbowify.cpp
@@ -1,11 +1,24 @@
 // gif_rainbowify.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+//#define TOUHOU_GIRL
+
+#ifdef TOUHOU_GIRL
+#define IMAGE "B:\\Users\\Idris\\Pictures\\1470984796120.gif"
+#define IMAGE_OUT "B:\\Users\\Idris\\Pictures\\1470984796120_rainbowed.gif"
+#define COL_FROM "blue"
+#define COL_TO "cyan"
+
+#else
+
+#define COL_FROM "dim gray"
+#define COL_TO "white"
 #define IMAGE "B:\\Users\\Idris\\Pictures\\1489175712017a.gif"
 #define IMAGE_OUT "B:\\Users\\Idris\\Pictures\\1489175712017a_cropped.gif"
+#endif
+
 #define RAINBOW_IMAGE_PATH "C:\\Users\\Idris\\source\\repos\\gif_rainbowify\\rainbow.png"
-
-
+#define NUM_HUE_VALUES 200
 
 #include <Magick++.h>
 #include <iostream> 
@@ -13,23 +26,22 @@
 using namespace std;
 using namespace Magick;
 
+
 int main(int argc, char** argv)
 {
     InitializeMagick(*argv);
 
-    // Construct the image object. Seperating image construction from the 
-    // the read operation ensures that a failure to read the image file 
-    // doesn't render the image object useless. 
     list<Image> imageList;
+    list<Image> newGif;
 
-    // TODO define rainbow image based on page size here, and crop it where it is currently defined.
+    // If this is ticked, all areas that were previously transparent will be rainbowified in addition to all areas that match the chroma key.
+    // however, when false its still a little broken - the rainbowified areas from the previous frame aren't copied over to the current frame, meaning
+    // they have the wrong hue.
+    bool makeTransparentRainbow = false;
     
     try {
         // Read each frame of the GIF 
         readImages(&imageList, IMAGE);
-
-        // build a new image but with the different height
-        list<Image> newGif;
 
         // Load a rainbow image from a file.
         // Resize it to match the image dimensions of the gif image we are loading.
@@ -43,39 +55,66 @@ int main(int argc, char** argv)
 
         // Define chosen colour range for transparency (e.g. which pixels will be rainbowified:
         // NOTE: transparencyChroma is only really suitable for colours that are similar (e.g. are monotonic across channels).
-        Color startColour = Color("dim gray"), endColour = Color("white");
+        Color startColour = Color(COL_FROM), endColour = Color(COL_TO);
         double hue = 100;
+        double huePerImage = NUM_HUE_VALUES / imageList.size();
+
+        Image prevImage = imageList.front();
         
         for (Image image : imageList) {
-            // We want the sections that were originally transparent to remain transparent.
-            // We can do this by CopyOpacityCompositeOp to copy the opacity from the original image 
-            Image originalImage = image;
+            Image combinedImage;
+            // To do this, we want to effectively play back the gif so we don't rainbowify bits we don't want to.
+            // Animated Gifs have a setting called dispose type that defines how the layers are going to be combined. http://www.theimage.com/animation/pages/disposal2.html
+            // Hence, to 'play' back the gif, we need to check the dispose type of the previous frame, and combine it based on the 
+            // defined dispose type:
+            switch (image.gifDisposeMethod()) {
+            case NoneDispose:
+            case UndefinedDispose:
+                // No disposal occurs. Each layer we need to do the union with the prevImage.
+                combinedImage = prevImage;
+                combinedImage.composite(image, SouthEastGravity, OverCompositeOp);
+                break;
+            case PreviousDispose:
+                // We dispose of the previous layer - we only keep this layer.
+                combinedImage = image;
+                break;
+            case BackgroundDispose:
+                // The background is set to a specified background colour (e.g. background transparency colour) before the image is overlayed on top of it.
+                combinedImage = Image(image.geometry(), image.backgroundColor());
+                combinedImage.composite(image, SouthEastGravity, OverCompositeOp);
+                break;
+           }
 
             // Make the sections of image we want transparent
-            image.transparentChroma(startColour, endColour);
+            Image diffImage = combinedImage;
+            combinedImage.transparentChroma(startColour, endColour);
 
             // Define the rainbow image. Note that the page size and the number of columns and rows are different. 
             Image rainbow_image = base_rainbow;
-            // Chagne the hue by 20%
+            // increment hue by number of frames
             rainbow_image.modulate(100, 100, hue);
-            hue+=20;
-
-            //rainbow_image.crop(image.page());
+            hue += huePerImage;
 
             // Copy the animation delay from the previous image
-            rainbow_image.animationDelay(originalImage.animationDelay());
+            rainbow_image.animationDelay(image.animationDelay());
 
-            // Crop/Scale the rainbow image to the size of the image.
-            rainbow_image.composite(image, SouthEastGravity, OverCompositeOp);
+            // Put the combined image on top of the rainbow image
+            rainbow_image.composite(combinedImage, SouthEastGravity, OverCompositeOp);
 
-            // Copy the original image opacity back so those sections aren't blue.
-            rainbow_image.composite(originalImage, SouthEastGravity, CopyOpacityCompositeOp);
+            // Copy the opacity values from the old combined Image so previously transparent areas aren't rainbowified (unless desired)
+            if (!makeTransparentRainbow) {
+                rainbow_image.composite(diffImage, SouthEastGravity, CopyOpacityCompositeOp);
+            }
 
             // Copy the original image page information so the image is drawn in the right place.
-            rainbow_image.page(originalImage.page());
+            rainbow_image.gifDisposeMethod(image.gifDisposeMethod());
+            rainbow_image.page(image.page());
             rainbow_image.repage();
 
-            newGif.push_back(rainbow_image);
+            newGif.push_back(rainbow_image); 
+
+            // Update prevImage
+            prevImage = combinedImage;
         }
 
         // Write the image to a file 
